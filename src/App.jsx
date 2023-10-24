f5c5e7ac30802f6e5d29cb89df927a2a9d6f08e1
Tomas Ricaurte <tomiychiqui@gmail.com>
2023-10-24 13:41:12 -0500

    Solucion laboratorio hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react";
-import "./App.css";
-import { getColor } from "./color-service";
+  import { useEffect, useState } from "react";
+  import "./App.css";
+  import { getColor } from "./color-service";
 
-export function App() {
-  const [counter, setCounter] = useState(0);
-  const [color, setColor] = useState("red");
+  export function App() {
+    const [counter, setCounter] = useState(0);
+    const [color, setColor] = useState("red");
 
-  return (
-    <div>
-      <h1>Ada School - React Hooks</h1>
+useEffect(() => {
+  if (counter >= 3) {
+    getColor("blue").then((newColor) => {
+      setColor(newColor)
+    })
+  }
+}, [counter])
 
+    return (
       <div>
-        <span className={color}>{`Counter:${counter}`}</span>
-      </div>
+        <h1>Ada School - React Hooks</h1>
 
-      <button>Increase</button>
-      <button>Decrease</button>
-      <button>Reset</button>
-    </div>
-  );
-}
+        <div>
+          <span className={color}>{`Counter:${counter}`}</span>
+        </div>
+
+        <button onClick={() => setCounter(counter + 1)}>Increase</button>
+        <button onClick={() => setCounter(counter -1)}>Decrease</button>
+        <button onClick={() => setCounter(0)}>Reset</button>
+      </div>
+    );
+  }
